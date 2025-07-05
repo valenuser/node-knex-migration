f76dfbdf8e2e904a188278fd88a8229df617b7fb
Rodolfo Barbeito Rodríguez <rbarbeito@gmail.com>
2025-07-05 23:11:59 +0200

    Usando loggers de winston como ejemplo en db/knex.js

--- db/knex.js
+++ db/knex.js
@@ -1,15 +1,16 @@
 const knex = require('knex')
 const config = require('../knexfile')
+const logger=require('../config/loggerWinston')
 
 const db = knex(config.development)
 
 
 db.raw('SELECT 1')
   .then(() => {
-    console.log('Conectado a la base de datos MySQL correctamente');
+    logger.info('Conectado a la base de datos MySQL correctamente');
   })
   .catch((err) => {
-    console.error('Error al conectar a la base de datos:', err.message);
+    logger.error('Error al conectar a la base de datos:', err.message);
   });
 
 
